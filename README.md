7f069897320d063fdadb1819a36d8b72b16e4923
Jeffrey Linwood <jlinwood@gmail.com>
2018-04-03 09:19:52 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# client-quickstart-js
-Twilio Client Quickstart for Web/Javascript
+<a href="https://www.twilio.com">
+  <img src="https://static0.twilio.com/marketing/bundles/marketing/img/logos/wordmark-red.svg" alt="Twilio" width="250" />
+</a>
+
+
+This application should give you a ready-made starting point for writing your
+own voice web applications with Twilio Client. Before we begin, we need to collect
+all the config values we need to run the application:
+
+| Config&nbsp;Value  | Description |
+| :-------------  |:------------- |
+TwiML&nbsp;App&nbsp;SID | The TwiML application with a voice URL configured to access your server running this app - create one [in the console here](https://www.twilio.com//console/phone-numbers/dev-tools/twiml-apps). Also, you will need to configure the Voice "REQUEST URL" on the TwiML app to point to a Twilio Function you will create.
+Twilio&nbsp;Phone&nbsp;# | A Twilio phone number in [E.164 format](https://en.wikipedia.org/wiki/E.164) - you can [get one here](https://www.twilio.com/console/phone-numbers/incoming)
+
+## Creating the quickstart functions
+
+To get you up and running with this quickstart, you can use a template to create your functions. Start by going to the [Twilio Functions](https://www.twilio.com/console/runtime/functions/manage) page in the Twilio Console.
+
+You can add new functions with the plus button on that screen. Look for the *Twilio Client Quickstart* template, and create it. You'll need the two values you gathered above as the configuration settings for this template.
+
+After you successfully put in the configuration, you'll have two Twilio Functions - one for the capability token, and one for the TwiML web application's voice response.
+
+## Configuration and setup
+
+[Configure your TwiML app](https://www.twilio.com/console/phone-numbers/dev-tools/twiml-apps)'s
+Voice "REQUEST URL" to be your Twilio Function URL - you can copy and paste the URL directly from the voice calls function page. For example:
+
+![screenshot of twiml app](https://s3.amazonaws.com/com.twilio.prod.twilio-docs/images/ClientJSCopyVoiceURL.width-500.png)
+
+Also, you'll need to modify the function URL in quickstart.js to point to your capability token Function - go ahead and copy the capability token URL like you did for the voice calls URL, and put that into quickstart.js, replacing this line:
+
+```
+https://YOUR_FUNCTION_SUBDOMAIN_HERE.twilio.io/capability-token
+```
+
+The subdomain you use for Twilio Functions will be the same for any functions you create - including the capability token and voice call functions.
+
+## Setting up a local HTTP server
+
+You'll need to load the front end of your web application from a web server for Twilio Client to connect successfully from Chrome (it should work in Firefox if you load it from the file system). The easiest way to do that is to install a local HTTP server like [http-server](https://github.com/indexzero/http-server)
+
+```bash
+npm install http-server -g
+```
+
+or 
+
+```bash
+sudo npm install http-server -g
+```
+
+on macOS or Linux
+
+
+## Run the local server
+
+```bash
+http-server
+```
+
+## Have some fun
+
+ You should now be ready to rock! Make some phone calls.
+   Open it on another device and call yourself. Note that Twilio Client requires
+   WebRTC enabled browsers, so Edge and Internet Explorer will not work for
+   testing. We'd recommend Google Chrome or Mozilla Firefox instead.
+
+   ![screenshot of chat app](https://s3.amazonaws.com/com.twilio.prod.twilio-docs/images/TwilioClientQuickstart.original.png)
+
+## Meta
+
+* No warranty expressed or implied. Software is as is. Diggity.
+* [MIT License](http://www.opensource.org/licenses/mit-license.html)
+* Lovingly crafted by Twilio Developer Education.
---
+++ public/quickstart.js
@@ -0,0 +1,166 @@
+﻿$(function () {
+  var speakerDevices = document.getElementById('speaker-devices');
+  var ringtoneDevices = document.getElementById('ringtone-devices');
+  var outputVolumeBar = document.getElementById('output-volume');
+  var inputVolumeBar = document.getElementById('input-volume');
+  var volumeIndicators = document.getElementById('volume-indicators');
+
+  log('Requesting Capability Token...');
+  $.getJSON('https://YOUR_FUNCTION_SUBDOMAIN_HERE.twilio.io/capability-token')
+    .done(function (data) {
+      log('Got a token.');
+      console.log('Token: ' + data.token);
+
+      // Setup Twilio.Device
+      Twilio.Device.setup(data.token);
+
+      Twilio.Device.ready(function (device) {
+        log('Twilio.Device Ready!');
+        document.getElementById('call-controls').style.display = 'block';
+      });
+
+      Twilio.Device.error(function (error) {
+        log('Twilio.Device Error: ' + error.message);
+      });
+
+      Twilio.Device.connect(function (conn) {
+        log('Successfully established call!');
+        document.getElementById('button-call').style.display = 'none';
+        document.getElementById('button-hangup').style.display = 'inline';
+        volumeIndicators.style.display = 'block';
+        bindVolumeIndicators(conn);
+      });
+
+      Twilio.Device.disconnect(function (conn) {
+        log('Call ended.');
+        document.getElementById('button-call').style.display = 'inline';
+        document.getElementById('button-hangup').style.display = 'none';
+        volumeIndicators.style.display = 'none';
+      });
+
+      Twilio.Device.incoming(function (conn) {
+        log('Incoming connection from ' + conn.parameters.From);
+        var archEnemyPhoneNumber = '+12099517118';
+
+        if (conn.parameters.From === archEnemyPhoneNumber) {
+          conn.reject();
+          log('It\'s your nemesis. Rejected call.');
+        } else {
+          // accept the incoming connection and start two-way audio
+          conn.accept();
+        }
+      });
+
+      setClientNameUI(data.identity);
+
+      Twilio.Device.audio.on('deviceChange', updateAllDevices);
+
+      // Show audio selection UI if it is supported by the browser.
+      if (Twilio.Device.audio.isSelectionSupported) {
+        document.getElementById('output-selection').style.display = 'block';
+      }
+    })
+    .fail(function () {
+      log('Could not get a token from server!');
+    });
+
+  // Bind button to make call
+  document.getElementById('button-call').onclick = function () {
+    // get the phone number to connect the call to
+    var params = {
+      To: document.getElementById('phone-number').value
+    };
+
+    console.log('Calling ' + params.To + '...');
+    Twilio.Device.connect(params);
+  };
+
+  // Bind button to hangup call
+  document.getElementById('button-hangup').onclick = function () {
+    log('Hanging up...');
+    Twilio.Device.disconnectAll();
+  };
+
+  document.getElementById('get-devices').onclick = function() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(updateAllDevices);
+  };
+
+  speakerDevices.addEventListener('change', function() {
+    var selectedDevices = [].slice.call(speakerDevices.children)
+      .filter(function(node) { return node.selected; })
+      .map(function(node) { return node.getAttribute('data-id'); });
+    
+    Twilio.Device.audio.speakerDevices.set(selectedDevices);
+  });
+
+  ringtoneDevices.addEventListener('change', function() {
+    var selectedDevices = [].slice.call(ringtoneDevices.children)
+      .filter(function(node) { return node.selected; })
+      .map(function(node) { return node.getAttribute('data-id'); });
+    
+    Twilio.Device.audio.ringtoneDevices.set(selectedDevices);
+  });
+
+  function bindVolumeIndicators(connection) {
+    connection.volume(function(inputVolume, outputVolume) {
+      var inputColor = 'red';
+      if (inputVolume < .50) {
+        inputColor = 'green';
+      } else if (inputVolume < .75) {
+        inputColor = 'yellow';
+      }
+
+      inputVolumeBar.style.width = Math.floor(inputVolume * 300) + 'px';
+      inputVolumeBar.style.background = inputColor;
+
+      var outputColor = 'red';
+      if (outputVolume < .50) {
+        outputColor = 'green';
+      } else if (outputVolume < .75) {
+        outputColor = 'yellow';
+      }
+
+      outputVolumeBar.style.width = Math.floor(outputVolume * 300) + 'px';
+      outputVolumeBar.style.background = outputColor;
+    });
+  }
+
+  function updateAllDevices() {
+    updateDevices(speakerDevices, Twilio.Device.audio.speakerDevices.get());
+    updateDevices(ringtoneDevices, Twilio.Device.audio.ringtoneDevices.get());
+  }
+});
+
+// Update the available ringtone and speaker devices
+function updateDevices(selectEl, selectedDevices) {
+  selectEl.innerHTML = '';
+  Twilio.Device.audio.availableOutputDevices.forEach(function(device, id) {
+    var isActive = (selectedDevices.size === 0 && id === 'default');
+    selectedDevices.forEach(function(device) {
+      if (device.deviceId === id) { isActive = true; }
+    });
+
+    var option = document.createElement('option');
+    option.label = device.label;
+    option.setAttribute('data-id', id);
+    if (isActive) {
+      option.setAttribute('selected', 'selected');
+    }
+    selectEl.appendChild(option);
+  });
+}
+
+// Activity log
+function log(message) {
+  var logDiv = document.getElementById('log');
+  logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
+  logDiv.scrollTop = logDiv.scrollHeight;
+}
+
+// Set the client name in the UI
+function setClientNameUI(clientName) {
+  var div = document.getElementById('client-name');
+  div.innerHTML = 'Your client name: <strong>' + clientName +
+    '</strong>';
+}
